bbd80c9901b043daa09ed3fd8054ada2c2461d9b
PavelKhromovAndrS <pavelkhromov1993@yandex.ru>
2022-06-25 19:02:05 +0300

    Added base layout

--- app/src/main/java/ru/pavelkhromov/githubapp/MainActivity.kt
+++ app/src/main/java/ru/pavelkhromov/githubapp/MainActivity.kt
@@ -2,10 +2,15 @@ package ru.pavelkhromov.githubapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import ru.pavelkhromov.githubapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
